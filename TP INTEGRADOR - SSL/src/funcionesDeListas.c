f3cadf43eb719adcc161c37ebb9acad38d8a7828
matiaspar2000 <matipartarrie@gmail.com>
2020-11-01 12:07:19 -0300

    funciones de listas

--- TP INTEGRADOR - SSL/src/funcionesDeListas.c
+++ TP INTEGRADOR - SSL/src/funcionesDeListas.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
 
-typedef struct variable {
+typedef struct listaDeVariables {
     char tipoDeDato[10];
     char nombreV[10];
     char valor[10];
-}variable;
-
-typedef struct funcion{
-    char tipoDeDatoSalida[10];
-    char nombreF[10];
-    struct listaDeVariables *parametros[10];
-}funcion;
-
-typedef struct listaDeVariables {
-    struct variable var;
     struct listaDeVariables *siguiente;
 }listaDeVariables;
 
 typedef struct listaDeFunciones{
-    struct funcion func;
+    char tipoDeDatoSalida[10];
+    char nombreF[10];
+    struct listaDeVariables *parametros[10];
     struct listaDeFunciones *siguiente;
 }listaDeFunciones;
+
+
+void reportarVariablesDeclaradas(listaDeVariables *TSVar){
+    while(TSVar->siguiente != NULL){
+        printf("Variable declarada: %s - ", TSVar->nombreV);
+        printf("Tipo: %s \n", TSVar->tipoDeDato);
+        TSVar = TSVar->siguiente;
+    }  
+}
+
+void reportarFuncionesDeclaradas(listaDeFunciones *TSFunc){
+    while(TSFunc->siguiente != NULL){
+        printf("Funcion declarada: %s - ", TSFunc->nombreF);
+        printf("Tipo que devuelve: %s - ", TSFunc->tipoDeDatoSalida);
+        printf("Cantidad de parametros: %s \n", contarParametros(TSFunc->parametros));
+        printf("Parametros: %s \n");
+        reportarVariablesDeclaradas(TSFunc->parametros);
+        TSFunc = TSFunc->siguiente;
+    }       
+}
+
+int contarParametros(listaDeVariables *parametros){
+    int contador = 0;
+    while (parametros->siguiente != NULL){
+        contador = contador + 1;
+        parametros = parametros->siguiente;
+    }  
+    return contador;
+}
 /*
 void insertarVariableUnica(variable nueva, listaDeVariables *TS){
 
 }
 
 void insertarFuncionUnica(funcion nueva, listaDeFunciones *TS){
-
-}
-
-void reportarVariablesDeclaradas(listaDeVariables *TS){
-
-}
-
-void reportarFuncionesDeclaradas(listaDeFunciones *TS){
 
 }*/
