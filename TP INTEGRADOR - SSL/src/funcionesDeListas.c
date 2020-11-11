f4b02f5bc46da9547167ac57228ef35b7a5e98c9
matiaspar2000 <matipartarrie@gmail.com>
2020-11-11 11:29:33 -0300

    funcion que devuelve el tipo de dato

--- TP INTEGRADOR - SSL/src/funcionesDeListas.c
+++ TP INTEGRADOR - SSL/src/funcionesDeListas.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "../src/tpINT.y"
 typedef struct listaDeVariables {
     char tipoDeDato[20];
     char nombreV[20];
@@ -67,3 +67,47 @@ int insertarFuncionUnica(listaDeFunciones *nueva, listaDeFunciones *TS){
    TS->siguiente = nueva;
    return 0;
 }  
+
+char* detectarTipoDeDatoVariableDeclarada(char nombreVariable[20], listaDeVariables *TS){
+  while (TS->siguiente != NULL){
+        if(TS->nombreV == nombreVariable){
+            printf("La variable es de tipo %s \n", TS->tipoDeDato);
+            return TS->tipoDeDato;
+        }
+        TS = TS->siguiente;
+   };
+   printf("ERROR SEMANTICO: La variable no fue declarada todavia");
+   return "noDeclarado";
+}
+
+char* detectarTipoDeDatoVariableNoDeclarada(char valor[20]){
+    int i=0;
+    int longitudValor= strlen(valor);
+    while(isdigit(valor[i])!=0){
+        i++;
+    };
+    if (longitudValor==i){
+        printf("La variable es de tipo INT %s \n");
+        return "INT";
+    }else if(valor[i] = "."){
+        printf("La variable es de tipo FLOAT %s \n");
+        return "FLOAT";
+    }else if(longitudValor = 1 && isdigit(valor[i])==0){
+        printf("La variable es de tipo CHAR %s \n");
+        return "CHAR";
+    }else if(longitudValor > 1 && isdigit(valor[i])==0){
+        longitudValor = 1;
+        printf("La variable es de tipo STRING %s \n");
+        return "STRING";
+    };
+}
+
+char* detectarTipoDeDato(char variableOvalor[]){
+    char tipoDeDato = detectarTipoDeDatoVariableDeclarada(variableOvalor,TS);
+    if(tipoDeDato = "noDeclarado"){
+        tipoDeDato = detectarTipoDeDatoVariableNoDeclarada(variableOvalor);
+        return tipoDeDato;
+    }else{
+        return tipoDeDato;
+    }   
+}
